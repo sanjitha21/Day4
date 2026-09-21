b4b0ef7a2e02c6963cc061feab01577313ec4eda
Sanjitha <ssanjitha3@gmail.com>
2026-09-21 12:09:03 +0530

    app41.py

--- app42.py
+++ app42.py
@@ -1,10 +1,4 @@
-# Exception handling in Python
-# It helps us manage runtime errors without stopping the program.
-# try: code that may raise an exception
-# except: handles the exception
-# else: runs only if no exception occurs
-# finally: runs no matter what
-
+#exception handling
 print("Exception handling example:\n")
 
 try:
@@ -19,11 +13,4 @@ else:
 finally:
     print("This block always executes.")
 
-# Another example with a custom error scenario
-print("\nAnother example:\n")
 
-try:
-    marks = [90, 80, 70]
-    print(marks[5])
-except IndexError:
-    print("Error: Index is out of range.")
