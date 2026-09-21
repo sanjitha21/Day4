e971ebe20f43c98b01ecb60b0a09cda99e771f5c
Sanjitha <ssanjitha3@gmail.com>
2026-09-21 12:10:07 +0530

    app42.py

--- app43.py
+++ app43.py
@@ -1,9 +1,6 @@
-# Class concept in Python
-# A class is a blueprint for creating objects.
-# It defines attributes (data) and methods (functions) that the objects will have.
-
+# Class 
 class Car:
-    # __init__ is a special method called when an object is created.
+   
     def __init__(self, brand, model, year):
         self.brand = brand   # instance attribute
         self.model = model
@@ -15,8 +12,6 @@ class Car:
     def info(self):
         return f"Car: {self.brand} {self.model}, Year: {self.year}"
 
-
-# Creating objects (instances) of the class
 car1 = Car("Toyota", "Corolla", 2022)
 car2 = Car("Honda", "Civic", 2023)
 
@@ -24,8 +19,3 @@ print(car1.info())
 print(car1.start_engine())
 print(car2.info())
 
-# Explanation:
-# - Car is a class.
-# - car1 and car2 are objects created from the Car class.
-# - Each object has its own attribute values.
-# - Methods are actions that objects can perform.
